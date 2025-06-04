96495cbf1af4974c16959438c3b3e5f28a1607b1
Ambrosini Nico (IT-PTR-CEN1-BDE4) <nico.ambrosini@sbb.ch>
2025-06-04 12:25:06 +0200

    feat: Add shorthand flags for commit command KAFKA-1838
    Details:
    - Modified the commit command to include shorthand flags for issue (-i), mode (-m), templateFile (-f), and commitType (-t)
    - Updated the flag definitions to use StringVarP instead of StringVar for shorthand support

--- cmd/commit.go
+++ cmd/commit.go
@@ -44,9 +44,9 @@ var commitCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(commitCmd)
 
-	rootCmd.PersistentFlags().StringVar(&issue, "issue", "", "the issue that should be incluted in the commit description (default is read from the TICKET env variable)")
-	rootCmd.PersistentFlags().StringVar(&mode, "mode", "concise", "if the commit text will be concise (one line) or full (one line with text), default is concise")
-	rootCmd.PersistentFlags().StringVar(&templateFile, "templateFile", "", "the file with the template to be used by the llm, the content will be sent directly to the llm (mode will be ignored if this flag is set)")
-	rootCmd.PersistentFlags().StringVar(&commitType, "type", "feat", "the type of the commit to be used as prefix of commit summary (feat, fix, etc.)")
+	rootCmd.PersistentFlags().StringVarP(&issue, "issue", "i", "", "the issue that should be incluted in the commit description (default is read from the TICKET env variable)")
+	rootCmd.PersistentFlags().StringVarP(&mode, "mode", "m", "concise", "if the commit text will be concise (one line) or full (one line with text), default is concise")
+	rootCmd.PersistentFlags().StringVarP(&templateFile, "templateFile", "f", "", "the file with the template to be used by the llm, the content will be sent directly to the llm (mode will be ignored if this flag is set)")
+	rootCmd.PersistentFlags().StringVarP(&commitType, "type", "t", "feat", "the type of the commit to be used as prefix of commit summary (feat, fix, etc.)")
 
 }
